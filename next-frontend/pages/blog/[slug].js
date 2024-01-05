0a41f0c1712dc21a6b14fd3e872f1d2f41585f42
Kamal Anand <kanand032003@gmail.com>
2024-01-05 22:17:56 +0530

    update

--- next-frontend/pages/blog/[slug].js
+++ next-frontend/pages/blog/[slug].js
@@ -40,19 +40,19 @@ const Post = ({ blog, profile }) => {
 
         <link crossOrigin="anonymous" href="https://fonts.gstatic.com" rel="preconnect" />
 
-        <link as="style" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap" rel="preload" />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap" as="style" />
 
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap" as="style" />
 
-        <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+        <link rel="preload" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" as="style" />
 
-        <link crossOrigin="anonymous" href="/assets/styles/main.min.css" media="screen" rel="stylesheet" />
+        <link crossOrigin="anonymous" href="/assets/styles/main.min.css" media="screen" rel="preload" as="style" />
 
         <script defer src="https://unpkg.com/@alpine-collective/toolkit@1.0.0/dist/cdn.min.js"></script>
 
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/atom-one-dark.min.css" />
+        <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/atom-one-dark.min.css" as="style" />
       </Head>
 
       <NavBar profile={profile} />
